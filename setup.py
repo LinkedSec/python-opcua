7dca2bbe2d2863f8957b033c77cb69e68ceb0fef
Anthony Oliver <anthony@sightmachine.com>
2016-06-20 16:12:59 -0400

    Updating to reflect as our fork

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ else:
     install_requires = []
 
 setup(name="freeopcua", 
-      version="0.10.12",
+      version="0.10.12-sm",
       description="Pure Python OPC-UA client and server library",
       author="Olivier Roulet-Dubonnet",
       author_email="olivier.roulet@gmail.com",
